7ffce2122490f0ebcdf3a71bdb639803b98d3091
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-04-02 18:04:13 -0400

    adding homescreen ui

--- ClassProgressTracker/Assets/Scripts/CourseManager.cs
+++ ClassProgressTracker/Assets/Scripts/CourseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EvtSystem;
 
 public class CourseManager : Singleton<CourseManager>
 {
@@ -27,22 +28,22 @@ public class CourseManager : Singleton<CourseManager>
 
     public void AddCourse(AddCourseTrigger evtData)
     {
-        // FIXME: create a CourseData scriptable object and add the given info
-        return;
-    }
-
-    public void RemoveCourse(RemoveCourseTrigger evtData)
-    {
-        // FIXME: check if course exists, then delete scriptable object and all assignments associated with it
-        
         // create a list of the keys
         List<string> courseDatabaseTitles = new List<string>(courseDatabase.Keys);
         // Check if course already exists
         if (courseDatabaseTitles.Contains(evtData.title)){
             Debug.Log("Course already exists");
             return;
+        }else{
+            Debug.Log("All good");
         }
-        
+
+        return;
+    }
+
+    public void RemoveCourse(RemoveCourseTrigger evtData)
+    {
+        // FIXME: check if course exists, then delete scriptable object and all assignments associated with it
         return;
     }
 }
